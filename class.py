d74fc16c66b30cff458644b626d8ce20ec28fe21
Faitus Jeline Joseph <65098339+faitusjelinej@users.noreply.github.com>
2024-04-11 01:38:47 -0700

    Create class.py

---
+++ class.py
@@ -0,0 +1,20 @@
+class House:
+    def __init__(self,color, owner,location,price):
+        self.color = color
+        self.owner = owner
+        self.location = location
+        self.price = price
+
+    def __str__(self):
+        return (f"{self.owner} owns the house located in {self.location} painted {self.color} spent {self.price}")    
+
+    def setincrease(self, percentage):
+        self.price = self.price + self.price * percentage * .01
+        return self.price
+
+owner1 = House("pink","David","NY",1000)
+owner2 = House("green","Edward","CA",5000)
+owner2.setincrease(5)
+
+print(owner1)
+print(owner2)
